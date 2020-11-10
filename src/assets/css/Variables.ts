9abfff447d7983df6509ae645cb41cf63f68209b
gustavograeff <gustavograeff1998@gmail.com>
2020-11-10 01:34:27 -0300

    add: color pallete

--- src/assets/css/Variables.ts
+++ src/assets/css/Variables.ts
@@ -5,3 +5,11 @@ export const smallFontSize = '12px';
 export const regularFontSize = '16px';
 export const bigFontSize = '18px';
 export const biggestFontSize = '20px';
+
+export const lightRose = '#efc7c2';
+export const lightBeige = '#ffe5d4';
+export const lightGreen = '#bfd3c1';
+export const mediumGreen = '#68a691';
+export const darkGreen = '#4D806F';
+export const lightPurple = '#694f5d';
+export const darkGrey = '#424B54';
