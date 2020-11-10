4b15cb7a1215827e5253101d5f44af4ebaa7c4f0
gustavograeff <gustavograeff1998@gmail.com>
2020-11-10 20:41:31 -0300

    add: some css variables

--- src/assets/css/Variables.ts
+++ src/assets/css/Variables.ts
@@ -6,10 +6,16 @@ export const regularFontSize = '16px';
 export const bigFontSize = '18px';
 export const biggestFontSize = '20px';
 
-export const lightRose = '#efc7c2';
 export const lightBeige = '#ffe5d4';
+export const lightRose = '#efc7c2';
 export const lightGreen = '#bfd3c1';
 export const mediumGreen = '#68a691';
 export const darkGreen = '#4D806F';
 export const lightPurple = '#694f5d';
 export const darkGrey = '#424B54';
+export const black = '#000000';
+
+export const zIndexBiggest = 9;
+export const zIndexSmallest = 1;
+
+export const backgroundOpacity = 0.5;
