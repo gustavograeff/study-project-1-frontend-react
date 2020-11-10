8fd626490dd95d265f36e2bdb44266b9939a16ac
gustavograeff <gustavograeff1998@gmail.com>
2020-11-09 21:50:56 -0300

    add: css variables

---
+++ src/assets/css/Variables.ts
@@ -0,0 +1,7 @@
+export const standardFontWeight = 500;
+export const strongFontWeight = 700;
+
+export const smallFontSize = '12px';
+export const regularFontSize = '16px';
+export const bigFontSize = '18px';
+export const biggestFontSize = '20px';
